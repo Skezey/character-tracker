718d2b99064b39b28d66b0989f2396680320d521
Scott Young <youngscottj@gmail.com>
2020-01-14 03:46:50 -0700

    add header

---
+++ src/components/header.js
@@ -0,0 +1,43 @@
+
+import React from 'react';
+import { useAuth0 } from '../contexts/auth0-context';
+import './Header.css'
+
+export default function Header() {
+  const { isLoading, user, loginWithRedirect, logout } = useAuth0();
+
+  return (
+    <header>
+      <nav className="navbar is-dark">
+        <div className="container">
+          <div className="navbar-menu is-active">
+            <div className="navbar-brand">
+              <button className="navbar-item">My Cool App!</button>
+            </div>
+
+            <div className="navbar-end">
+
+              {!isLoading && !user && (
+                <button onClick={loginWithRedirect} className="navbar-item">
+                  Login
+                </button>
+              )}
+
+              {!isLoading && user && (
+                <>
+                  <button className="navbar-item">{user.name}</button>
+                  <button
+                    onClick={() => logout({ returnTo: window.location.origin })}
+                    className="navbar-item"
+                  >
+                    Logout
+                  </button>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
